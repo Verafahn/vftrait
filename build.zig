52749e1da467167b734457181db0fd7a312b434d
Verafahn <WeeiyihFahn@outlook.com>
2026-05-20 15:52:02 +0800

    Create root module and re-point module source to it

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
 
     const vftrait = b.addModule("vftrait", .{
-        .root_source_file = b.path("src/trait.zig"),
+        .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
