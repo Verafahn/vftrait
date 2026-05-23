dbb61b30ba1f5d9e5ccb7a7e10ef18b1eb8335b5
Verafahn <WeeiyihFahn@outlook.com>
2026-05-23 11:22:38 +0800

    Fix: Correct the judgment logic of the self parameter.

--- src/matching.zig
+++ src/matching.zig
@@ -136,7 +136,8 @@ pub fn matchMethods(comptime Trait: type, comptime T: type) bool {
                         @typeInfo(TraitSelf).pointer
                     else
                         break :blk false;
-                    copy_info.child = trait_self_info.child;
+                    if (copy_info.child == anyopaque)
+                        copy_info.child = trait_self_info.child;
                     if (!std.meta.eql(copy_info, trait_self_info))
                         break :blk false;
                 },
